e21bfdf8bbcd8f337a18a87b53b04a6ca1d979f6
Florin Holhos <florin.holhos@softvision.ro>
2019-12-27 14:59:08 +0200

    initial commit

---
+++ desktop_weather_app/desktop_weather_app.h
@@ -0,0 +1,32 @@
+
+// desktop_weather_app.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CdesktopweatherappApp:
+// See desktop_weather_app.cpp for the implementation of this class
+//
+
+class CdesktopweatherappApp : public CWinApp
+{
+public:
+	CdesktopweatherappApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CdesktopweatherappApp theApp;
